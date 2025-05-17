0093ef707ec3bea8976c5a5c382a8343d0399017
Cach13 <119624326+Cach13@users.noreply.github.com>
2025-05-16 23:00:27 -0600

    Add files via upload
    Actualizacion de la BD.sql nuevamente

---
+++ cronograma (3).sql
@@ -0,0 +1,361 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-05-2025 a las 06:59:30
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cronograma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `diasnohabiles`
+--
+
+CREATE TABLE `diasnohabiles` (
+  `id_dia` int(11) NOT NULL,
+  `id_semestre` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `disponibilidad`
+--
+
+CREATE TABLE `disponibilidad` (
+  `id_disponibilidad` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dosificacion`
+--
+
+CREATE TABLE `dosificacion` (
+  `id_dosificacion` int(11) NOT NULL,
+  `id_tema` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `horas_asignadas` decimal(4,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluaciones`
+--
+
+CREATE TABLE `evaluaciones` (
+  `id_evaluacion` int(11) NOT NULL,
+  `id_unidad` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fecha_evaluacion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programas`
+--
+
+CREATE TABLE `programas` (
+  `id_programa` int(11) NOT NULL,
+  `id_semestre` int(11) NOT NULL,
+  `nombre_materia` varchar(100) NOT NULL,
+  `horas_teoricas` int(11) DEFAULT NULL,
+  `horas_practicas` int(11) DEFAULT NULL,
+  `id_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `semestres`
+--
+
+CREATE TABLE `semestres` (
+  `id_semestre` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `id_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temas`
+--
+
+CREATE TABLE `temas` (
+  `id_tema` int(11) NOT NULL,
+  `id_unidad` int(11) NOT NULL,
+  `nombre_tema` varchar(100) DEFAULT NULL,
+  `horas_estimadas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `unidades`
+--
+
+CREATE TABLE `unidades` (
+  `id_unidad` int(11) NOT NULL,
+  `id_programa` int(11) NOT NULL,
+  `nombre_unidad` varchar(100) DEFAULT NULL,
+  `numero_unidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `rol` enum('admin','alumno') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacaciones`
+--
+
+CREATE TABLE `vacaciones` (
+  `id_lapso` int(11) NOT NULL,
+  `id_semestre` int(11) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `descripcion` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `diasnohabiles`
+--
+ALTER TABLE `diasnohabiles`
+  ADD PRIMARY KEY (`id_dia`),
+  ADD KEY `id_semestre` (`id_semestre`);
+
+--
+-- Indices de la tabla `disponibilidad`
+--
+ALTER TABLE `disponibilidad`
+  ADD PRIMARY KEY (`id_disponibilidad`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `dosificacion`
+--
+ALTER TABLE `dosificacion`
+  ADD PRIMARY KEY (`id_dosificacion`),
+  ADD KEY `id_tema` (`id_tema`);
+
+--
+-- Indices de la tabla `evaluaciones`
+--
+ALTER TABLE `evaluaciones`
+  ADD PRIMARY KEY (`id_evaluacion`),
+  ADD KEY `id_unidad` (`id_unidad`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `programas`
+--
+ALTER TABLE `programas`
+  ADD PRIMARY KEY (`id_programa`),
+  ADD KEY `id_semestre` (`id_semestre`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indices de la tabla `semestres`
+--
+ALTER TABLE `semestres`
+  ADD PRIMARY KEY (`id_semestre`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indices de la tabla `temas`
+--
+ALTER TABLE `temas`
+  ADD PRIMARY KEY (`id_tema`),
+  ADD KEY `id_unidad` (`id_unidad`);
+
+--
+-- Indices de la tabla `unidades`
+--
+ALTER TABLE `unidades`
+  ADD PRIMARY KEY (`id_unidad`),
+  ADD KEY `id_programa` (`id_programa`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  ADD PRIMARY KEY (`id_lapso`),
+  ADD KEY `id_semestre` (`id_semestre`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `diasnohabiles`
+--
+ALTER TABLE `diasnohabiles`
+  MODIFY `id_dia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `disponibilidad`
+--
+ALTER TABLE `disponibilidad`
+  MODIFY `id_disponibilidad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `dosificacion`
+--
+ALTER TABLE `dosificacion`
+  MODIFY `id_dosificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `evaluaciones`
+--
+ALTER TABLE `evaluaciones`
+  MODIFY `id_evaluacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `programas`
+--
+ALTER TABLE `programas`
+  MODIFY `id_programa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `semestres`
+--
+ALTER TABLE `semestres`
+  MODIFY `id_semestre` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `temas`
+--
+ALTER TABLE `temas`
+  MODIFY `id_tema` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `unidades`
+--
+ALTER TABLE `unidades`
+  MODIFY `id_unidad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  MODIFY `id_lapso` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `diasnohabiles`
+--
+ALTER TABLE `diasnohabiles`
+  ADD CONSTRAINT `diasnohabiles_ibfk_1` FOREIGN KEY (`id_semestre`) REFERENCES `semestres` (`id_semestre`);
+
+--
+-- Filtros para la tabla `disponibilidad`
+--
+ALTER TABLE `disponibilidad`
+  ADD CONSTRAINT `disponibilidad_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `dosificacion`
+--
+ALTER TABLE `dosificacion`
+  ADD CONSTRAINT `dosificacion_ibfk_1` FOREIGN KEY (`id_tema`) REFERENCES `temas` (`id_tema`);
+
+--
+-- Filtros para la tabla `evaluaciones`
+--
+ALTER TABLE `evaluaciones`
+  ADD CONSTRAINT `evaluaciones_ibfk_1` FOREIGN KEY (`id_unidad`) REFERENCES `unidades` (`id_unidad`),
+  ADD CONSTRAINT `evaluaciones_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `programas`
+--
+ALTER TABLE `programas`
+  ADD CONSTRAINT `programas_ibfk_1` FOREIGN KEY (`id_semestre`) REFERENCES `semestres` (`id_semestre`),
+  ADD CONSTRAINT `programas_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `semestres`
+--
+ALTER TABLE `semestres`
+  ADD CONSTRAINT `semestres_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `temas`
+--
+ALTER TABLE `temas`
+  ADD CONSTRAINT `temas_ibfk_1` FOREIGN KEY (`id_unidad`) REFERENCES `unidades` (`id_unidad`);
+
+--
+-- Filtros para la tabla `unidades`
+--
+ALTER TABLE `unidades`
+  ADD CONSTRAINT `unidades_ibfk_1` FOREIGN KEY (`id_programa`) REFERENCES `programas` (`id_programa`);
+
+--
+-- Filtros para la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  ADD CONSTRAINT `vacaciones_ibfk_1` FOREIGN KEY (`id_semestre`) REFERENCES `semestres` (`id_semestre`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
